fd8c7b999dea5db0f9d7077cef25d64c49d33285
MiryamBathilde <mbathilde@gmail.com>
2022-07-20 19:13:20 +0200

    Exercise session 1 done

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿/*
+ * Ejercicio 1
+   Escribe un programa que reciba tu nombre y lo escriba por consola.
+*/
+
+//Nombre?
+Console.WriteLine("Introduce tu nombre: ");
+string nombre = Console.ReadLine();
+
+//Imprimir en pantalla
+Console.WriteLine("Hola " + nombre);
+
+/*
+ * Ejercicio 2
+   Escribe un programa que tome la hora y la escriba por pantalla.
+*/
+
+// Hora actual
+string dateTime = DateTime.Now.ToString("HH:MM:ss");
+Console.WriteLine("Hora local: " + dateTime);
